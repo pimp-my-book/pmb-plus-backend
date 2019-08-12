af8018ffc5dbae270470311e16f478c204ddbebe
amodinho <u14284783@tuks.co.za>
2019-08-12 14:39:22 +0200

    added vendor table

--- tables.sql
+++ tables.sql
@@ -21,6 +21,13 @@ CREATE TABLE IF NOT EXISTS vendor
 (
  vendor_id  MEDIUMINT UNSIGNED not null AUTO_INCREMENT,
  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ vendor_name varchar(250) not null,
+ vendor_description varchar(250),
+ vendor_website varchar(250),
+ vendor_address varchar(250),
+ vendor_email varchar(250),
+ vendor_phone_number varchar(250),
+ user_id varchar(250),
  vendor_type_id MEDIUMINT UNSIGNED not null,
  FOREIGN KEY(vendor_type_id) REFERENCES vendor_type(vendor_type_id) ON UPDATE CASCADE ON DELETE RESTRICT,
 );
