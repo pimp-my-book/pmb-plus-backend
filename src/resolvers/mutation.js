772b34f20d0e505ad670e37b432c02ea0ed94e45
amodinho <u14284783@tuks.co.za>
2019-08-14 15:16:19 +0200

    done with mutation

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -15,10 +15,27 @@ try{
 
    let newVendor = await db.query(`INSERT INTO vendor (vendor_name,vendor_description,vendor_website, vendor_address,vendor_email,vendor_phone_number,user_id,vendor_type_id ) VALUES(?,?,?,?,?,?,?,?,?)`,
 [
-    
-]
-)
-} catch(e){
+  addVendorInput.vendorName,
+  addVendorInput.vendorDescription,
+  addVendorInput.vendorWebsite,
+  addVendorInput.vendorAddreess,
+  addVendorInput.vendorEmail,
+  addVendorInput.vendorPhone,
+  '94c3ae75-5a32-4c44-bc17-e80cbfc006a7',
+  2
+])
+console.log(newVendor)
+await db.end()
 
+return {
+    vendorName: args.vendorName,
+    vendorDescription:args.vendorDescription,
+    vendorWebsite: args.vendorWebsite,
+    vendorEmail: args.vendorEmail,
+    vendorPhone: args.vendorPhone,
+    vendorAddreess: argsvendorAddreess,
+}
+} catch(e){
+   return e
 }
 }
