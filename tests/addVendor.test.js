9663358bcf5d8e307498dc9a8a21624d68c74c8f
amodinho <u14284783@tuks.co.za>
2019-08-14 14:30:58 +0200

    added skeleton for test

--- tests/addVendor.test.js
+++ tests/addVendor.test.js
@@ -1,1 +1,15 @@
 //Test for addVendor Mutation
+describe('Add a vendor', ()=>{
+
+    it("Adds a new vendor successfull",()=>{
+
+    })
+
+    it("Needs a userID", ()=>{
+
+    })
+
+    it("Needs a name", ()=>{
+        
+    })
+})
