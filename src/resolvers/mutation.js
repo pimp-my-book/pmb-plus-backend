3af05cfd2f2d4ad56856396c7c3b2655cc2ee837
amodinho <u14284783@tuks.co.za>
2019-08-14 14:40:28 +0200

    added sql values

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,6 +1,24 @@
+//All the apps mutations are here
 import db from '../../libs/db'
 
 //Add Vendor
 export const addVendor = async ({input:args}, context) =>{
+try{
+   const addVendorInput = {
+    vendorName: args.vendorName,
+    vendorDescription:args.vendorDescription,
+    vendorWebsite: args.vendorWebsite,
+    vendorEmail: args.vendorEmail,
+    vendorPhone: args.vendorPhone,
+    vendorAddreess: argsvendorAddreess,
+   }
 
+   let newVendor = await db.query(`INSERT INTO vendor (vendor_name,vendor_description,vendor_website, vendor_address,vendor_email,vendor_phone_number,user_id,vendor_type_id ) VALUES(?,?,?,?,?,?,?,?,?)`,
+[
+    
+]
+)
+} catch(e){
+
+}
 }
