1b6ac94038c72fc3fe8c101fd0123e95c256fab0
amodinho <u14284783@tuks.co.za>
2019-08-14 15:18:40 +0200

    added it to the index of ythe resoolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,7 +1,12 @@
-import {hello} from "./query";
+import {hello} from './query';
+import {addVendor} from './mutation';
 
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
+    },
+    Mutation: {
+        addVendor:(root, {input:args}, context) => addVendor({input:args}, context)
+        
     }
 }
