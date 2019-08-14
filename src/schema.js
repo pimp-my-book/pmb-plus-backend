515e0ed59e68fc6d0748f86378d80d851b3618b5
amodinho <u14284783@tuks.co.za>
2019-08-14 14:12:28 +0200

    added newschema

--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,103 @@
 const schema = `
-
-
-
 """
-A hello world Query
+Add Book input
 """
+input addBookInput {
+    
+    productName: String!
+    productDescription: String!
+    
+    price: String!
+    vendor: String!
+    image: String!
+    edition: String!
+    title: String!
+    author: String!
+    ISBN: String!
+    grade: String!
+  
+}
+"""
+Add Vendor Input
+"""
+input addVendorInput {
+    vendorName: String!
+    vendorDescription: String!
+    vendorWebsite: String
+    vendorAddress: String!
+    vendorEmail: String!
+}
+"""
+Buying List Type
+"""
+type BuyingList {
+    ID: Int!
+    store: String!
+    books:[Book]!
+}
+"""
+Vendor Type
+"""
+type Vendor {
+    ID: Int!
+    vendorName: String!
+    vendorDescription: String!
+    vendorWebsite: String
+    vendorAddress: String!
+    vendorEmail: String!
+    inventory: [Product]!
+    buyingList: [BuyingList]!
+}
+"""
+Product interface
+"""
+interface Product {
+    ID: Int!
+    productName: String!
+    productDescription: String!
+    dateUploaded: String!
+    price: String!
+    productType: String!
+    vendor: String!
+    image: String!
+    
+}
+"""
+A book type 
+"""
+type Book  {
+    ID: Int!
+    dateUploaded: String!
+    price: String!
+    vendor: String!
+    image: String!
+    productType: String!
+    title: String!
+    author: String!
+    ISBN: String!
+    edition: String!
+    grade: String!
+   
+}
+type S3Payload {
+    signedRequest: String!,
+    url: String!
+}
+type Mutation {
+    
+    addBook(input: addBookInput) : Book
+    addVendor(input: addVendorInput): Vendor
+    addBooks(fileName: String!, fileType: String!): S3Payload!
+}
 type Query {
      hello: String!
+     connection: String
+     """ 
+     A query to get all the books a vendor has posted. 
+     """
+     getBooksByVendor(vendor:String!): [Book]
+     getAllBooks: [Book]
+     getOneBook(ID: Int): Book
 }
-
 `
-
 export {schema}
