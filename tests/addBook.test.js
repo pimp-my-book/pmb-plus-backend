2fd30b3bcaf466c9f65884965210e81ae5b0692a
AmoDinho <u14284783@tuks.co.za>
2019-10-01 16:14:30 +0200

    added a test case

---
+++ tests/addBook.test.js
@@ -0,0 +1,12 @@
+const resolvers = require('../src/resolvers/index')
+
+
+describe('Add a book mutation', () => {
+    it('Adds a book', () => {
+
+    })
+
+    it('shows an error', () => {
+
+    })
+})
