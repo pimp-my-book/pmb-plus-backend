258dfdf5502dd2003dd2444b8011887b7147923d
AmoDinho <u14284783@tuks.co.za>
2019-10-01 16:15:55 +0200

    added addBook mutation in index

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,12 +1,12 @@
-import {hello} from './query'
-import {addVendor} from './mutation'
+import { hello } from './query'
+import { addVendor, addBook } from './mutation'
 
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
     },
     Mutation: {
-        addVendor:(root, {input:args}, context) => addVendor({input:args}, context)
-
+        addVendor: (root, { input: args }, context) => addVendor({ input: args }, context),
+        addBook: (root, { input: args }, context) => addBook({ input: args }, context)
     }
 }
