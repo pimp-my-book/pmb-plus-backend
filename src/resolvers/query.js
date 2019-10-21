06e60fb06bcad67898080ebbda81b262d264ad1f
AmoDinho <u14284783@tuks.co.za>
2019-10-21 15:02:21 +0200

    adding context to see what i can use to determine the stage

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,3 +1,5 @@
 export const hello = (args, context) => {
-	return `PMB Plus' API is now LIVE!🎈 ${process.env.NODE_ENV}`
+	return `PMB Plus' API is now LIVE!🎈 ${process.env.NODE_ENV},
+	the context is ${context.event}
+	`
 }
