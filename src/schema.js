801a9237b86ca2d34eada2e8bc6ee7814155e2c9
AmoDinho <u14284783@tuks.co.za>
2019-10-22 14:53:33 +0200

    added the queries necessary

--- src/schema.js
+++ src/schema.js
@@ -55,18 +55,31 @@ type Query {
 A query that gets the latest books to be upload on the
 marketplace
    """
-     getLatestBooks: [Book!]!
+   getLatestBooks: [Book!]!
+
      """
 A query that gets books by degree and then catergorizes them by 
 whatever degrees the users put in
    """
      getBooksByDegree: [Book!]!
+
      """
 A query that gets books by course and then then catergorizes them by 
 whatever courses the users put in
    """
-     """
-     getBooksByCourse: [Book!]!
+    getBooksByCourse: [Book!]!
+
+    """
+A query that gets books by their lowest price range
+    """
+    getBooksByMinPrice: [Book!]!
+
+    """
+A query that gets books by univeristy
+    """
+    getBooksByUniversity: [Book!]!
+
+    
      getAllBooks: [Book]
      getOneBook(ID: Int): Book
 }
