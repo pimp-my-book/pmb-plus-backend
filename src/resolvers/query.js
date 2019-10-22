ed67dd8fa5bf983011d8daf6398857637b2beb65
AmoDinho <u14284783@tuks.co.za>
2019-10-22 14:56:49 +0200

    added skeleton resolvers

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -4,3 +4,25 @@ export const hello = (args, context) => {
 
 	return console.log(db) && console.log(JSON.parse(JSON.stringify(db)))
 }
+
+
+//getLatestBooks
+export const getLatestBooks = async (args, context) => {
+	return "null"
+}
+//getBooksByDegree
+export const getBooksByDegree = async (args, context) => {
+	return "null"
+}
+//getBooksByCourse
+export const getBooksByCourse = async (args, context) => {
+	return "null"
+}
+//getBooksByMinPrice
+export const getBooksByMinPrice = async (args, context) => {
+	return "null"
+}
+//getBooksByUniversity
+export const getBooksByUniversity = async (args, context) => {
+	return "null"
+}
