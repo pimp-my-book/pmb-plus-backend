1f389fc7e9dc9c0f8a326a15521f4975b90db8f8
AmoDinho <u14284783@tuks.co.za>
2019-10-22 14:59:46 +0200

    added all the queries

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -6,6 +6,11 @@ export const resolvers = {
 		hello: (root, args, context) => hello(args, context),
 		getLatestBooks: (root, args, context) => getLatestBooks(args, context),
 		getBooksByDegree: (root, args, context) => getBooksByDegree(args, context),
+		getBooksByCourse: (root, args, context) => getBooksByCourse(args, context),
+		getBooksByMinPrice: (root, args, context) => getBooksByMinPrice(args, context),
+
+		getBooksByUniversity: (root, args, context) => getBooksByUniversity(args, context),
+
 	},
 	Mutation: {
 
