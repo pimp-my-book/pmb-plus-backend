d2019ecbedfc9795e2db89bd486a8e39c10efa94
AmoDinho <u14284783@tuks.co.za>
2019-10-22 14:58:26 +0200

    imported queries

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,12 +1,14 @@
-import { hello } from './query'
+import { hello, getBooksByDegree, getLatestBooks, getBooksByCourse, getBooksByMinPrice, getBooksByUniversity } from './query'
 import { addBook } from './mutation'
 
 export const resolvers = {
-    Query: {
-        hello: (root, args, context) => hello(args, context)
-    },
-    Mutation: {
+	Query: {
+		hello: (root, args, context) => hello(args, context),
+		getLatestBooks: (root, args, context) => getLatestBooks(args, context),
+		getBooksByDegree: (root, args, context) => getBooksByDegree(args, context),
+	},
+	Mutation: {
 
-        addBook: (root, { input: args }, context) => addBook({ input: args }, context)
-    }
+		addBook: (root, { input: args }, context) => addBook({ input: args }, context)
+	}
 }
