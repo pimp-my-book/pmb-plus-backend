f97b497042ffdb7cab5080dc5be14dac6d8fa3b2
AmoDinho <u14284783@tuks.co.za>
2019-10-31 11:09:15 +0200

    exported getMyBooks to index.js

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,4 +1,4 @@
-import { hello, getOneBook, getBooksByDegree, getLatestBooks, getBooksByCourse, getBooksByMinPrice, getBooksByMaxPrice, getBooksByUniversity } from './query'
+import { hello, getOneBook, getMyBooks, getBooksByDegree, getLatestBooks, getBooksByCourse, getBooksByMinPrice, getBooksByMaxPrice, getBooksByUniversity } from './query'
 import { addBook, editBook } from './mutation'
 
 export const resolvers = {
@@ -10,7 +10,8 @@ export const resolvers = {
 		getBooksByMinPrice: (root, args, context) => getBooksByMinPrice(args, context),
 		getBooksByMaxPrice: (root, args, context) => getBooksByMaxPrice(args, context),
 		getBooksByUniversity: (root, args, context) => getBooksByUniversity(args, context),
-		getOneBook: (root, args, context) => getOneBook(args, context)
+		getOneBook: (root, args, context) => getOneBook(args, context),
+		getMyBooks: (root, args, context) => getMyBooks(args, context)
 
 	},
 	Mutation: {
